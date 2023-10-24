7862d0f7c6bdb1266d7ce07e1e572d1c6febda10
Renat <ontheflyflyfly@gmail.com>
2023-10-24 22:47:51 +0300

    add functions to the tableView: cells are generated cells are fetched new entry can be added to the list

--- Dictionary/Controller/FavoritesController.swift
+++ Dictionary/Controller/FavoritesController.swift
@@ -7,23 +7,65 @@
 
 import UIKit
 
-class FavoritesController: UIViewController, UITableViewDelegate {
+class FavoritesController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+    
     private var models = [FavoritesItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         title = "CoreData Test List"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+        tableView.backgroundColor = .systemGray4
+        getAllItems()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTap))
     }
 
+    @objc private func didTap() {
+        let alert = UIAlertController(title: "New Item", message: "Enter new word", preferredStyle: .alert)
+        
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self]_ in
+            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else {
+                return
+            }
+            self?.createItem(name: text)
+        }))
+        present(alert, animated: true)
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.word
+        return cell
+    }
+    
     // CoreData functions
     
     func getAllItems(){
         do {
             models = try context.fetch(FavoritesItem.fetchRequest())
             
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         } catch {
             
         }
