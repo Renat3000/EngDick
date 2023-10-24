89bede74734d33c3a8fb7790785232ba37c8e1e2
Renat <ontheflyflyfly@gmail.com>
2023-10-24 22:41:32 +0300

    start work on CoreData. CoreData will be used to save dictionary entries in user's list: introduce CoreDataModel add CoreData context functions to AppDelegate add new functions to FavoritesController

--- Dictionary/Controller/FavoritesController.swift
+++ Dictionary/Controller/FavoritesController.swift
@@ -7,12 +7,61 @@
 
 import UIKit
 
-class FavoritesController: UIViewController {
+class FavoritesController: UIViewController, UITableViewDelegate {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private var models = [FavoritesItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "CoreData Test List"
     }
 
-
+    // CoreData functions
+    
+    func getAllItems(){
+        do {
+            models = try context.fetch(FavoritesItem.fetchRequest())
+            
+        } catch {
+            
+        }
+    }
+    
+    func createItem(name: String){
+        let newItem = FavoritesItem(context: context)
+        newItem.word = name
+        newItem.dateOfCreation = Date()
+        
+        do {
+            try context.save()
+            getAllItems()
+        }
+        catch {
+            
+        }
+    }
+    
+    func deleteItem(item: FavoritesItem) {
+        context.delete(item)
+        
+        do {
+            try context.save()
+        }
+        catch {
+            
+        }
+    }
+    
+    func updateItem(item: FavoritesItem, newName: String) {
+        item.word = newName
+        
+        do {
+            try context.save()
+        }
+        catch {
+            
+        }
+    }
 }
