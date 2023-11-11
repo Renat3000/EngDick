c05bd00fe00e382f9ae55b1e3558afdb07e7465b
Renat <ontheflyflyfly@gmail.com>
2023-11-11 17:52:27 +0300

    fruitless endeavors with this tutorial: https://developer.apple.com/documentation/uikit/view_controllers/using_suggested_searches_with_a_search_controller
    guess I'm just stupid. need to change DictionaryController from UICollectionViewController to UiTableViewController and life will become more joyous

--- Dictionary/Controller/DictionaryController.swift
+++ Dictionary/Controller/DictionaryController.swift
@@ -10,7 +10,7 @@ import UIKit
 class DictionaryController: UICollectionViewController, UISearchBarDelegate, UICollectionViewDelegateFlowLayout, UITableViewDataSource, UITableViewDelegate {
 
     fileprivate let cellId = "dictionaryCell"
-    fileprivate let searchController = UISearchController(searchResultsController: nil)
+    var searchController: UISearchController!
     private var searchOptions: [String] = []
     private var filteredSearchOptions: [String] = []
     
@@ -32,6 +32,9 @@ class DictionaryController: UICollectionViewController, UISearchBarDelegate, UIC
     override func viewDidLoad() {
         super.viewDidLoad()
         self.collectionView.backgroundColor = .systemGray4
+        
+        searchController = UISearchController(searchResultsController: nil)
+        loadSearchOptions()
         loadSearchBar()
         collectionView.register(DictionaryEntryPreviewCell.self, forCellWithReuseIdentifier: cellId)
     }
@@ -41,9 +44,9 @@ class DictionaryController: UICollectionViewController, UISearchBarDelegate, UIC
         navigationItem.searchController = self.searchController
         navigationItem.hidesSearchBarWhenScrolling = false
         searchController.searchBar.delegate = self
-//        loadSearchOptions()
-        suggestionsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "suggestionCell")
-//        searchController.searchBar.addSubview(suggestionsTableView)
+        
+        
+        searchController.searchBar.addSubview(suggestionsTableView)
 //
 //        NSLayoutConstraint.activate([
 //            suggestionsTableView.topAnchor.constraint(equalTo: searchController.searchBar.bottomAnchor),
@@ -52,10 +55,8 @@ class DictionaryController: UICollectionViewController, UISearchBarDelegate, UIC
 //               suggestionsTableView.bottomAnchor.constraint(equalTo: searchController.searchBar.bottomAnchor)
 //        ])
 //
-//        // Устанавливаем делегат и источник данных для таблицы
-//        suggestionsTableView.delegate = self
-//        suggestionsTableView.dataSource = self
-
+        suggestionsTableView.delegate = self
+        suggestionsTableView.dataSource = self
     }
     
     fileprivate var JSONTopResult = [JSONStruct]()
@@ -131,24 +132,23 @@ class DictionaryController: UICollectionViewController, UISearchBarDelegate, UIC
           if let path = Bundle.main.path(forResource: "english", ofType: "txt"),
              let contents = try? String(contentsOfFile: path) {
               searchOptions = contents.components(separatedBy: .newlines)
-              print(searchOptions[0...4])
           }
     }
  
     // MARK: - UISearchBarDelegate
-//
-//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//        if !searchText.isEmpty {
-//            // Фильтруем варианты поиска на основе введенного текста
-//            filteredSearchOptions = searchOptions.filter { $0.lowercased().contains(searchText.lowercased()) }
-//            // Показываем таблицу с подсказками
-//            suggestionsTableView.isHidden = false
-//            suggestionsTableView.reloadData()
-//        } else {
-//            // Если текст поиска пуст, скрываем таблицу
-//            suggestionsTableView.isHidden = true
-//        }
-//    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if !searchText.isEmpty {
+            // Фильтруем варианты поиска на основе введенного текста
+            filteredSearchOptions = searchOptions.filter { $0.lowercased().contains(searchText.lowercased()) }
+            // Показываем таблицу с подсказками
+            suggestionsTableView.isHidden = false
+            suggestionsTableView.reloadData()
+        } else {
+            // Если текст поиска пуст, скрываем таблицу
+            suggestionsTableView.isHidden = true
+        }
+    }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
           if let searchText = searchBar.text {
@@ -164,7 +164,7 @@ class DictionaryController: UICollectionViewController, UISearchBarDelegate, UIC
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "suggestionCell?", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "suggestionCell", for: indexPath)
         cell.textLabel?.text = filteredSearchOptions[indexPath.row]
         return cell
     }
@@ -179,3 +179,50 @@ class DictionaryController: UICollectionViewController, UISearchBarDelegate, UIC
 //    }
     
 }
+
+//    fileprivate let cellId = "dictionaryCell"
+//    fileprivate var searchController = UISearchController(searchResultsController: nil)
+    
+//    init() {
+////        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+
+//  if we use collectionView: UICollectionViewController, UICollectionViewDelegateFlowLayout
+//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return JSONTopResult.count
+//    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! DictionaryEntryPreviewCell
+//
+//        cell.wordLabel.text = JSONTopResult[indexPath.item].word
+//        cell.phoneticsLabel.text = JSONTopResult[indexPath.item].phonetic
+//
+//        self.JSONMeanings = JSONTopResult[indexPath.item].meanings
+//        cell.partOfSpeechLabel1.text = JSONMeanings[0].partOfSpeech
+//
+//        cell.definitionLabel1.text = String()
+//        cell.definitionLabel1.text = JSONMeanings[0].definitions[0].definition
+//
+//        if JSONMeanings[0].definitions.count > 1 {
+//            cell.definitionLabel1.text?.append("..")
+//        }
+//
+//        return cell
+//    }
+    
+    //only available if we have UICollectionViewDelegateFlowLayout protocol
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: view.frame.width-10, height: 120)
+//    }
+    
+//    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//
+//        let selectedItem = JSONTopResult[indexPath.item]
+//        let wdController = WordDetailsController(item: selectedItem, isBookmarked: false)
+//        navigationController?.pushViewController(wdController, animated: true)
+//    }
