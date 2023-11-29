3145884d1fd6d625545c70b3ed6508ce5259ef52
Renat <ontheflyflyfly@gmail.com>
2023-11-29 18:36:51 +0300

    clean up the CardView (UIView)

--- Dictionary/Extension.swift
+++ Dictionary/Extension.swift
@@ -28,3 +28,37 @@ extension UIView {
         }
     }
 }
+
+func makeButtonForSets(withText text: String) -> UIButton {
+    let button = UIButton(type: .system)
+    
+    button.setTitle(text, for: .normal)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.layer.cornerRadius = 10
+    button.titleLabel?.textColor = .black
+    button.tintColor = .black
+    button.backgroundColor = .white
+    
+    return button
+}
+
+func makeLabel() -> UILabel {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.numberOfLines = 2
+    label.textColor = .white
+    label.backgroundColor = .systemGray4
+    label.font = .systemFont(ofSize: 24)
+    label.textAlignment = .center
+    return label
+}
+
+func  makeStackView() -> UIStackView {
+    let view = UIStackView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.distribution = .fill
+    view.alignment = .fill
+    view.spacing = 8.0
+    
+    return view
+}
--- Dictionary/View/CardView.swift
+++ Dictionary/View/CardView.swift
@@ -11,125 +11,15 @@ class CardView: UIView {
 
     weak var delegate: CardViewDelegate?
     
-    let stackViewVertical: UIStackView = {
-        let view = UIStackView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.axis = .vertical
-        view.distribution = .fill
-        view.alignment = .fill
-        view.spacing = 8.0
-        
-        return view
-    }()
-    
-    let wordStackViewVertical: UIStackView = {
-        let view = UIStackView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.axis = .vertical
-        view.distribution = .fill
-        view.alignment = .fill
-        view.spacing = 8.0
-        
-        return view
-    }()
-    
-    let wordLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.numberOfLines = 2
-        label.textColor = .white
-        label.backgroundColor = .systemGray4
-        label.font = .systemFont(ofSize: 36)
-        label.textAlignment = .center
-        return label
-    }()
-    
-    let definitionLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.numberOfLines = 0
-        label.textColor = .white
-        label.backgroundColor = .systemGray4
-        label.font = .systemFont(ofSize: 24)
-        label.textAlignment = .center
-        return label
-    }()
-    
-    let showAnswerButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.layer.cornerRadius = 10
-        button.setTitle("Show Answer", for: .normal)
-        button.titleLabel?.textColor = .black
-        button.tintColor = .black
-        button.backgroundColor = .white
-        
-        button.addTarget(self, action: #selector(didTapShowAnswerButton), for: .touchUpInside)
-        return button
-    }()
-    
-    let easyButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.layer.cornerRadius = 10
-        button.setTitle("Easy", for: .normal)
-        button.titleLabel?.textColor = .black
-        button.tintColor = .black
-        button.backgroundColor = .white
-        button.addTarget(self, action: #selector(didTapButtonStack), for: .touchUpInside)
-        
-        return button
-    }()
-    
-    let goodButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.layer.cornerRadius = 10
-        button.setTitle("Good", for: .normal)
-        button.titleLabel?.textColor = .black
-        button.tintColor = .black
-        button.backgroundColor = .white
-//            button.addTarget(self, action: #selector(didTapButtonStack), for: .touchUpInside)
-        
-        return button
-    }()
-    
-    let hardButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.layer.cornerRadius = 10
-        button.setTitle("Hard", for: .normal)
-        button.titleLabel?.textColor = .black
-        button.tintColor = .black
-        button.backgroundColor = .white
-//            button.addTarget(self, action: #selector(didTapButtonStack), for: .touchUpInside)
-        
-        return button
-    }()
-    
-    let againButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.layer.cornerRadius = 10
-        button.setTitle("Again", for: .normal)
-        button.titleLabel?.textColor = .black
-        button.tintColor = .black
-        button.backgroundColor = .white
-        button.addTarget(self, action: #selector(didTapButtonStack), for: .touchUpInside)
-        
-        return button
-    }()
-    
-    let buttonStackView: UIStackView = {
-        let view = UIStackView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.axis = .horizontal
-        view.distribution = .fillEqually
-        view.alignment = .fill
-        view.spacing = 8.0
-        
-        return view
-    }()
+    let wordStackViewVertical = makeStackView()
+    let wordLabel = makeLabel()
+    let definitionLabel = makeLabel()
+    let showAnswerButton = makeButtonForSets(withText: "Show Answer")
+    let easyButton = makeButtonForSets(withText: "Easy")
+    let goodButton = makeButtonForSets(withText: "Good")
+    let hardButton =  makeButtonForSets(withText: "Hard")
+    let againButton = makeButtonForSets(withText: "Again")
+    let buttonStackView = makeStackView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -142,36 +32,18 @@ class CardView: UIView {
     
     func setupView() {
         backgroundColor = .systemGray5
-        //old
-//        wordLabel.text = "some words"
-//        buttonStackView.addArrangedSubview(easyButton)
-//        buttonStackView.addArrangedSubview(goodButton)
-//        buttonStackView.addArrangedSubview(hardButton)
-//        buttonStackView.addArrangedSubview(againButton)
-//
-//        wordStackViewVertical.addArrangedSubview(wordLabel)
-//        wordStackViewVertical.addArrangedSubview(definitionLabel)
-//
-//        stackViewVertical.addArrangedSubview(wordStackViewVertical)
-//        stackViewVertical.addArrangedSubview(showAnswerButton)
-//        stackViewVertical.addArrangedSubview(buttonStackView)
-//        addSubview(stackViewVertical)
-//
-//        wordLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-//        wordLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -200).isActive = true
-//
-//        wordStackViewVertical.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-////        wordStackViewVertical.widthAnchor.constraint(equalToConstant: 150).isActive = true
-//
-////        showAnswerButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-////        showAnswerButton.widthAnchor.constraint(equalToConstant: 60).isActive = true
-////        wordStackViewVertical.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 30).isActive = true
-////        wordStackViewVertical.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30).isActive = true
-//
-//        stackViewVertical.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-//        stackViewVertical.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         
-        //new
+        wordLabel.numberOfLines = 2
+        definitionLabel.numberOfLines = 0
+        wordStackViewVertical.axis = .vertical
+        buttonStackView.axis = .horizontal
+        buttonStackView.distribution = .fillEqually
+        
+        showAnswerButton.addTarget(self, action: #selector(didTapShowAnswerButton), for: .touchUpInside)
+        easyButton.addTarget(self, action: #selector(didTapButtonStack), for: .touchUpInside)
+        goodButton.addTarget(self, action: #selector(didTapButtonStack), for: .touchUpInside)
+        hardButton.addTarget(self, action: #selector(didTapButtonStack), for: .touchUpInside)
+        againButton.addTarget(self, action: #selector(didTapButtonStack), for: .touchUpInside)
         
         let buttonHeight: CGFloat = 40
         
@@ -200,21 +72,6 @@ class CardView: UIView {
         buttonStackView.topAnchor.constraint(equalTo: showAnswerButton.bottomAnchor, constant: 20).isActive = true
         buttonStackView.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
         buttonStackView.widthAnchor.constraint(equalToConstant: 300).isActive = true
-        
-//        wordLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-//        wordLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -200).isActive = true
-//
-//        wordStackViewVertical.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-////        wordStackViewVertical.widthAnchor.constraint(equalToConstant: 150).isActive = true
-//
-////        showAnswerButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-////        showAnswerButton.widthAnchor.constraint(equalToConstant: 60).isActive = true
-////        wordStackViewVertical.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 30).isActive = true
-////        wordStackViewVertical.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30).isActive = true
-//
-//        stackViewVertical.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-//        stackViewVertical.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-        
     }
     
     @objc func didTapShowAnswerButton() {
